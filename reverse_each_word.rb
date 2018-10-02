4494b26383145333633cc573b276477c4ce367df
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 17:50:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 
 def reverse_each_word(string)
   words = string.split(" ")
-    new_string = words.map do |word|
+    new_string = words.collect do |word|
     word.reverse
   end
   new_string.join(" ")
